4fdb550cfbb6e7fecabc0e3929ba604b3bd5367a
Dean Norman <dean@22seven.com>
2018-08-02 16:13:00 +0200

    basic drag func

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
+import { DragDropContext } from 'react-beautiful-dnd';
 import accountCardData from './accountCardData';
 import Column from './column';
 
 class App extends React.Component {
    state = accountCardData;
 
+   onDragEnd = result => {
+
+   }
+
    render() {
-      return this.state.columnOrder.map((columnId) => {
-         const column = this.state.columns[columnId];
-         const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
-         
-         return <Column key={column.id} column={column} tasks={tasks} />
-      });
+      return(
+         <DragDropContext onDragEnd={this.onDragEnd}>
+            {this.state.columnOrder.map((columnId) => {
+               const column = this.state.columns[columnId];
+               const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+               return <Column key={column.id} column={column} tasks={tasks} />
+            })}
+         </DragDropContext>
+      );
    }
 }
 
--- src/task.jsx
+++ src/task.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -6,11 +7,24 @@ const Container = styled.div`
    border-radius: 2px;
    padding: 8px;
    margin-bottom: 8px;
+   background-color: #fff;
 `;
 
 export default class Task extends React.Component {
    render() {
-      console.log(this.props);
-      return <Container>{this.props.task.content}</Container>;
+      return (
+         <Draggable draggableId={this.props.task.id} index={this.props.index}>
+            {(provided) => (
+               <Container
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  innerRef={provided.innerRef}
+               >
+                  {this.props.task.content}
+               </Container>
+            )}
+            
+         </Draggable>
+      );
    }
 }
